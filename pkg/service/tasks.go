b408013a4781e7b1bd62f05755285a97a815addc
Marcos Huck <marcoshuck@ekumenlabs.com>
2023-11-01 22:42:13 -0300

    Add tracing based from context to CreateTask
    Signed-off-by: Marcos Huck <marcoshuck@ekumenlabs.com>

--- pkg/service/tasks.go
+++ pkg/service/tasks.go
@@ -5,6 +5,7 @@ import (
 	tasksv1 "github.com/marcoshuck/todo/api/tasks/v1"
 	"github.com/marcoshuck/todo/pkg/domain"
 	"go.opentelemetry.io/otel/metric"
+	"go.opentelemetry.io/otel/trace"
 	"go.uber.org/zap"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
@@ -22,18 +23,24 @@ type tasks struct {
 // CreateTask creates a Task.
 func (svc *tasks) CreateTask(ctx context.Context, request *tasksv1.CreateTaskRequest) (*tasksv1.Task, error) {
 	svc.logger.Debug("Creating task", zap.String("task.title", request.GetTask().GetTitle()))
+	span := trace.SpanFromContext(ctx)
+	defer span.End()
 
 	var task domain.Task
 	svc.logger.Debug("Filling out task information")
+	span.AddEvent("Parsing task from API request")
 	err := task.FromCreateRequest(request)
 	if err != nil {
 		svc.logger.Warn("User submitted an invalid task", zap.Error(err))
+		span.RecordError(err)
 		return nil, status.Errorf(codes.InvalidArgument, "invalid task: %s", err)
 	}
+	span.AddEvent("Persisting task in the database")
 	svc.logger.Debug("Persisting task in the database", zap.String("task.title", request.GetTask().GetTitle()))
 	err = svc.db.Model(&domain.Task{}).WithContext(ctx).Create(&task).Error
 	if err != nil {
 		svc.logger.Error("Failed to create task", zap.Error(err))
+		span.RecordError(err)
 		return nil, status.Errorf(codes.Unavailable, "failed to create task")
 	}
 	svc.logger.Debug("Returning created task", zap.String("task.title", request.GetTask().GetTitle()))
