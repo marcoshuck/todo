e63097364b5b7f209e6bfe30bfb297b1dea67859
Marcos Huck <marcos@huck.com.ar>
2024-02-12 17:55:19 -0300

    Init E2E tests with playwright
    Signed-off-by: Marcos Huck <marcos@huck.com.ar>

---
+++ tests/tasks.writer.spec.ts
@@ -0,0 +1,22 @@
+import {expect, test} from '@playwright/test';
+import {Task} from '../gen/typescript/api/tasks/v1/tasks_pb';
+
+test('POST /v1/tasks', async ({request}) => {
+    let input = {
+        title: 'An awesome task',
+        description: 'An awesome description for an awesome task',
+    };
+
+    const response = await request.post('/v1/tasks', {
+        data: input,
+    });
+    expect(response.ok()).toBeTruthy();
+
+    const body = await response.body();
+    const output: Task = Task.fromBinary(body);
+
+    expect(output.title).toEqual(input.title);
+    expect(output.description).toEqual(input.description);
+    expect(output.createTime).not.toBeNull();
+    expect(output.updateTime).not.toBeNull();
+})
