dd601f54c6067d4611de14e946d4d324cd0b8bc8
Marcos Huck <marcos@huck.com.ar>
2024-02-12 18:44:11 -0300

    Add get task test
    Signed-off-by: Marcos Huck <marcos@huck.com.ar>

--- tests/tasks.writer.spec.ts
+++ tests/tasks.writer.spec.ts
@@ -21,3 +21,28 @@ test('POST /v1/tasks', async ({request}) => {
     expect(output.createTime).not.toBeNull();
     expect(output.updateTime).not.toBeNull();
 })
+
+test('GET /v1/tasks/:id', async ({request}) => {
+    let input = {
+        title: 'An awesome task',
+        description: 'An awesome description for an awesome task',
+    };
+
+    const created = await request.post('/v1/tasks', {
+        data: input,
+    });
+    expect(created.ok()).toBeTruthy();
+
+    let body = await created.body();
+    const expected: Task = Task.fromJsonString(body.toString());
+
+    const response = await request.get(`/v1/tasks/${expected.id}`)
+    body = await response.body();
+    const output: Task = Task.fromJsonString(body.toString());
+
+    expect(output.title).toEqual(expected.title);
+    expect(output.description).toEqual(expected.description);
+    expect(output.id).toEqual(expected.id);
+    expect(output.createTime).toEqual(expected.createTime);
+    expect(output.updateTime).toEqual(expected.updateTime);
+})
