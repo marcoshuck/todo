07f5f4fdfa1526dda8a69e741f1140ec867a880f
Marcos Huck <marcos@huck.com.ar>
2024-02-12 00:14:59 -0300

    Add Typescript and Dart generation
    Signed-off-by: Marcos Huck <marcos@huck.com.ar>

--- api/tasks/v1/tasks.pb.gw.go
+++ api/tasks/v1/tasks.pb.gw.go
@@ -35,11 +35,7 @@ func request_TasksWriterService_CreateTask_0(ctx context.Context, marshaler runt
 	var protoReq CreateTaskRequest
 	var metadata runtime.ServerMetadata
 
-	newReader, berr := utilities.IOReaderFactory(req.Body)
-	if berr != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
-	}
-	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq.Task); err != nil && err != io.EOF {
+	if err := marshaler.NewDecoder(req.Body).Decode(&protoReq.Task); err != nil && err != io.EOF {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 
@@ -52,11 +48,7 @@ func local_request_TasksWriterService_CreateTask_0(ctx context.Context, marshale
 	var protoReq CreateTaskRequest
 	var metadata runtime.ServerMetadata
 
-	newReader, berr := utilities.IOReaderFactory(req.Body)
-	if berr != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
-	}
-	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq.Task); err != nil && err != io.EOF {
+	if err := marshaler.NewDecoder(req.Body).Decode(&protoReq.Task); err != nil && err != io.EOF {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 
@@ -121,11 +113,7 @@ func request_TasksWriterService_UndeleteTask_0(ctx context.Context, marshaler ru
 	var protoReq UndeleteTaskRequest
 	var metadata runtime.ServerMetadata
 
-	newReader, berr := utilities.IOReaderFactory(req.Body)
-	if berr != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
-	}
-	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
+	if err := marshaler.NewDecoder(req.Body).Decode(&protoReq); err != nil && err != io.EOF {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 
@@ -138,11 +126,7 @@ func local_request_TasksWriterService_UndeleteTask_0(ctx context.Context, marsha
 	var protoReq UndeleteTaskRequest
 	var metadata runtime.ServerMetadata
 
-	newReader, berr := utilities.IOReaderFactory(req.Body)
-	if berr != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
-	}
-	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
+	if err := marshaler.NewDecoder(req.Body).Decode(&protoReq); err != nil && err != io.EOF {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 
@@ -152,7 +136,7 @@ func local_request_TasksWriterService_UndeleteTask_0(ctx context.Context, marsha
 }
 
 var (
-	filter_TasksWriterService_UpdateTask_0 = &utilities.DoubleArray{Encoding: map[string]int{"task": 0, "id": 1}, Base: []int{1, 4, 5, 2, 0, 0, 0, 0}, Check: []int{0, 1, 1, 2, 4, 2, 2, 3}}
+	filter_TasksWriterService_UpdateTask_0 = &utilities.DoubleArray{Encoding: map[string]int{"task": 0, "id": 1}, Base: []int{1, 2, 1, 0, 0}, Check: []int{0, 1, 2, 3, 2}}
 )
 
 func request_TasksWriterService_UpdateTask_0(ctx context.Context, marshaler runtime.Marshaler, client TasksWriterServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
