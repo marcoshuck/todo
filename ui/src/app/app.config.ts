fc8b4bdc5f78d1cd273ed966d53cd3c4c1ec2425
Marcos Huck <marcos@huck.com.ar>
2024-08-05 00:29:44 -0300

    Install Material and Buf

--- ui/src/app/app.config.ts
+++ ui/src/app/app.config.ts
@@ -3,7 +3,8 @@ import {provideRouter} from '@angular/router';
 
 import {routes} from './app.routes';
 import {provideClientHydration} from '@angular/platform-browser';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({eventCoalescing: true}), provideRouter(routes), provideClientHydration()]
+  providers: [provideZoneChangeDetection({eventCoalescing: true}), provideRouter(routes), provideClientHydration(), provideAnimationsAsync()]
 };
